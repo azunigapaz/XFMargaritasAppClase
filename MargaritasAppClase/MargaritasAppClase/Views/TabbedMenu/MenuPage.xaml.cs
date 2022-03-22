717b41778823740ef41d5d7dae167ae3d807b7d4
Alejandro <azunigapaz@gmail.com>
2022-03-22 00:19:24 -0600

    Get Products API, Done

--- MargaritasAppClase/MargaritasAppClase/Views/TabbedMenu/MenuPage.xaml.cs
+++ MargaritasAppClase/MargaritasAppClase/Views/TabbedMenu/MenuPage.xaml.cs
@@ -7,19 +7,53 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using MargaritasAppClase.Controller;
+using MargaritasAppClase.Models;
+using Xamarin.Essentials;
+
 namespace MargaritasAppClase.Views.TabbedMenu
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MenuPage : ContentPage
     {
+
         public MenuPage()
         {
             InitializeComponent();
+            GetProductsList();
         }
 
         private void btnagregarcarrito_Clicked(object sender, EventArgs e)
         {
 
         }
+
+        private async void GetProductsList()
+        {
+            var AccesoInternet = Connectivity.NetworkAccess;
+
+            if (AccesoInternet == NetworkAccess.Internet)
+            {
+                sl.IsVisible = true;
+                spinner.IsRunning = true;
+
+                List<ProductsListModel> listaproducros = new List<ProductsListModel>();
+                listaproducros = await ProductsApiController.ControllerObtenerListaProductos();
+
+                if (listaproducros.Count > 0)
+                {
+                    listview_mainproductos.ItemsSource = null;
+                    listview_mainproductos.ItemsSource = listaproducros;
+                }
+                else
+                {
+                    await DisplayAlert("Notificación", $"Lista vacía, ingrese datos", "Ok");
+                }
+
+                sl.IsVisible = false;
+                spinner.IsRunning = false;
+            }
+        }
+
     }
 }
