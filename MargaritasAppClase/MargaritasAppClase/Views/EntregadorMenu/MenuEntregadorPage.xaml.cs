78b05cd2ab81557eaaef840bfc17febd72e9c426
Jose Vigil <jvigil504@gmail.com>
2022-04-05 22:40:28 -0600

    Creacion de Interfaz de Repartidor

---
+++ MargaritasAppClase/MargaritasAppClase/Views/EntregadorMenu/MenuEntregadorPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MargaritasAppClase.Views.EntregadorMenu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuEntregadorPage : ContentPage
+    {
+        public MenuEntregadorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnverorden_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncambiarstatusorden_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
