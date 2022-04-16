59327b6d3d53cff5a1cdf96b8e879d28f7abfa6a
Jose Vigil <jvigil504@gmail.com>
2022-04-15 21:34:49 -0600

    Icono App

--- MargaritasAppClase/MargaritasAppClase.Android/MainActivity.cs
+++ MargaritasAppClase/MargaritasAppClase.Android/MainActivity.cs
@@ -15,7 +15,7 @@ using Plugin.LocalNotification;
 
 namespace MargaritasAppClase.Droid
 {
-    [Activity(Label = "MargaritasAppClase", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "MargaritasAppClase", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
