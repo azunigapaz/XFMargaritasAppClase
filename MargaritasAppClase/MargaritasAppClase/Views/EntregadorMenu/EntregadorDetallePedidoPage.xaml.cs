5dfe7a0641217284204e061b5e3a93490ac4c633
Jose Vigil <jvigil504@gmail.com>
2022-04-17 12:39:21 -0600

    Diseño Detalle Perfil Cliente, Detalle Pedido Entregador y Seguir Ubicacion Pedido.

---
+++ MargaritasAppClase/MargaritasAppClase/Views/EntregadorMenu/EntregadorDetallePedidoPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MargaritasAppClase.Views.EntregadorMenu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntregadorDetallePedidoPage : ContentPage
+    {
+        public EntregadorDetallePedidoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnescucharaudioreferenciaentregador_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
